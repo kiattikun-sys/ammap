7673a1fa2ec653952d6b008ea50b95e9eb15678b
mek <tprwebhelper@gmail.com>
2026-03-11 10:07:06 +0700

    chore: remove .next from tracking

--- .next/static/chunks/app/(project)/[projectId]/work/page-1bdfa3ce77bcad8b.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[286,931,23,695,985,566,548,71,422,614,265,795,930,709,569,442,432,86],{3521:function(){}},function(n){n.O(0,[971,582,744],function(){return n(n.s=3521)}),_N_E=n.O()}]);
--- .next/static/chunks/app/layout-a4c0d85b3d0c1ab2.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[185],{2390:function(n,e,u){Promise.resolve().then(u.t.bind(u,5204,23))},5204:function(){}},function(n){n.O(0,[846,971,582,744],function(){return n(n.s=2390)}),_N_E=n.O()}]);
